aae9c3069ee844ed28565d356b038f790bfa9478
Denys Bondarchuk <bondarchuk520@gmail.com>
2019-10-08 10:09:49 +0300

    Updates

---
+++ precache-manifest.f547748a57a2185a6ab30c0de528bfdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7064f445982524d63dc292d05ece0d58",
+    "url": "/React-RubicksTimer/index.html"
+  },
+  {
+    "revision": "d649189195b1d45f6543",
+    "url": "/React-RubicksTimer/static/css/main.800ccf47.chunk.css"
+  },
+  {
+    "revision": "59df0de30cb2f39b0851",
+    "url": "/React-RubicksTimer/static/js/2.ef89e0c8.chunk.js"
+  },
+  {
+    "revision": "d649189195b1d45f6543",
+    "url": "/React-RubicksTimer/static/js/main.8ade5992.chunk.js"
+  },
+  {
+    "revision": "e1bd9d7eeb7fd5ec6440",
+    "url": "/React-RubicksTimer/static/js/runtime-main.84c14d79.js"
+  },
+  {
+    "revision": "ff3bdfe36a86e1ff09f0521ff75c3546",
+    "url": "/React-RubicksTimer/static/media/arrow.ff3bdfe3.svg"
+  },
+  {
+    "revision": "e0d25831d09c2248ae402024f0f15708",
+    "url": "/React-RubicksTimer/static/media/delete-red.e0d25831.svg"
+  }
+]);
